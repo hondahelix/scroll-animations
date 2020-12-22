26277e843e2a85388b91f77f615e3ca8a5149e5c
Jonathan Honda <hondahelix120@gmail.com>
2020-12-21 19:10:11 -0800

    added an edited video with both gifs

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const firstGif = document.querySelector('.first-gif');
 const   firstScene = firstGif.querySelector('video');
 //end section
-const section = document.querySelector('section');
-const end = section.querySelector('.second-gif')
+//const section = document.querySelector('section');
+//const end = section.querySelector('.second-gif')
 
 const controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
-    duration: 7200,
+    duration: 11000,
     triggerElement: firstGif,
     triggerHook: 0
 })
@@ -15,20 +15,24 @@ const scene = new ScrollMagic.Scene({
 .setPin(firstGif)
 .addTo(controller)
 
-
-
 //video animation 
 let accelamount = 0.1;
 let scrollpos =0;
 let delay = 0;
+let interval = 100;
+// let buttonPress = true;
 
 scene.on('update', e =>{
     scrollpos = e.scrollPos/1000;
+    console.log(scrollpos);
 });
-
+//becasue other gif was 50fps it changes interval from 100 to 50 when it gets to it
+if(delay >9.25){
+    interval = 50;
+}
 setInterval(()=>{
+    //delays the scroll so it catches up when it stops
     delay += (scrollpos - delay) * accelamount;
-    //console.log(delay);
     firstScene.currentTime = delay;
-    //fames/1000 
-},100);
+},interval);
+
